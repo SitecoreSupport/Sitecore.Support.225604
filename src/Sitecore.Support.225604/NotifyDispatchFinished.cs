76fc9ead5033948a1b134c18a94c56707d8d0a26
Sergey Plashenko <spl@sitecore.net>
2018-06-20 15:03:27 +0300

    Fix for the issue

--- src/Sitecore.Support.225604/NotifyDispatchFinished.cs
+++ src/Sitecore.Support.225604/NotifyDispatchFinished.cs
@@ -1,43 +1,55 @@
-﻿namespace Sitecore.Modules.EmailCampaign.Core.Pipelines.DispatchNewsletter
+﻿using System;
+using System.Reflection;
+using Sitecore.Diagnostics;
+using Sitecore.EmailCampaign.Model;
+using Sitecore.EmailCampaign.Model.Dispatch;
+using Sitecore.ExM.Framework.Diagnostics;
+using Sitecore.Modules.EmailCampaign.Core;
+using Sitecore.Modules.EmailCampaign.Core.Pipelines.DispatchNewsletter;
+using Sitecore.Modules.EmailCampaign.Factories;
+
+namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.DispatchNewsletter
 {
-    using Sitecore.Diagnostics;
-    using Sitecore.EmailCampaign.Model;
-    using Sitecore.EmailCampaign.Model.Dispatch;
-    using Sitecore.ExM.Framework.Diagnostics;
-    using Sitecore.Modules.EmailCampaign.Core;
-    using System;
+  public class NotifyDispatchFinished
+  {
+    private readonly CoreFactory coreFactory;
+    private readonly ILogger logger;
 
-    public class NotifyDispatchFinished
+    public NotifyDispatchFinished() : this(typeof(BusinessLogicFactory).GetProperty("CoreFactory", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(EcmFactory.GetDefaultFactory().Bl) as CoreFactory, EcmFactory.GetDefaultFactory().Io.Logger)
     {
-        private readonly CoreFactory coreFactory;
-        private readonly ILogger logger;
-
-        public NotifyDispatchFinished() : this(EcmFactory.GetDefaultFactory().Bl.CoreFactory, EcmFactory.GetDefaultFactory().Io.Logger)
-        {
-        }
-
-        public NotifyDispatchFinished(CoreFactory coreFactory, ILogger logger)
-        {
-            Assert.ArgumentNotNull(coreFactory, "coreFactory");
-            Assert.ArgumentNotNull(logger, "logger");
-            this.coreFactory = coreFactory;
-            this.logger = logger;
-        }
-
-        public void Process(DispatchNewsletterArgs args)
-        {
-            if ((!args.IsTestSend && args.AllowNotifications) && (args.Message.EnableNotifications && (args.DispatchInterruptRequest != DispatchInterruptSignal.Pause)))
-            {
-                try
-                {
-                    string str = args.SendingAborted ? EcmTexts.Localize("Aborted", new object[0]) : EcmTexts.Localize("Completed", new object[0]);
-                    this.coreFactory.GetNotification(args.Message).SendDispatchFinished(str.ToLower());
-                }
-                catch (Exception exception)
-                {
-                    this.logger.LogError(exception);
-                }
-            }
-        }
     }
+
+    public NotifyDispatchFinished(CoreFactory coreFactory, ILogger logger)
+    {
+      Assert.ArgumentNotNull(coreFactory, "coreFactory");
+      Assert.ArgumentNotNull(logger, "logger");
+      this.coreFactory = coreFactory;
+      this.logger = logger;
+    }
+
+    public void Process(DispatchNewsletterArgs args)
+    {
+      if ((!args.IsTestSend && args.AllowNotifications) && (args.Message.EnableNotifications && (args.DispatchInterruptRequest != DispatchInterruptSignal.Pause)))
+      {
+        try
+        {
+          string str;
+          if (args.SendingAborted || !args.RequireFinalMovement)
+          {
+            str = EcmTexts.Localize("Aborted", new object[0]);
+          }
+          else
+          {
+            str = EcmTexts.Localize("Completed", new object[0]);
+          }
+
+          this.coreFactory.GetNotification(args.Message).SendDispatchFinished(str.ToLower());
+        }
+        catch (Exception exception)
+        {
+          this.logger.LogError(exception);
+        }
+      }
+    }
+  }
 }
